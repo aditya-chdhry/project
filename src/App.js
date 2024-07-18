1042a22b651fa35c8bb8ed6abed1ce96afeeb836
Aditya Chaudhary <145284918+aditya-chdhry@users.noreply.github.com>
2024-07-18 15:26:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/Homepage.jsx';
+import InstructorSection from './components/Instructorsec.jsx';
+import Relatedsec from './components/Relatedsec.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import Videosection from './components/Videosection.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <InstructorSection/>
+        <Relatedsec/>
+        <Sidebar/>
+        <Videosection/>
+        <App/>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          {/* Add routes for other pages here */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
