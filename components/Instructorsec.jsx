23de906c9cf9e2e356a737374172f3a84d959f56
Aditya Chaudhary <145284918+aditya-chdhry@users.noreply.github.com>
2024-07-18 15:18:51 +0530

    Create Instructorsec.jsx

---
+++ components/Instructorsec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Instructorsec.css';
+
+const Instructorsec = () => {
+  return (
+    <div className="instructor-section">
+      <h3>Instructor</h3>
+      <div className="instructor-info">
+        <img src="instructor-image-url" alt="Instructor" />
+        <div>
+          <h4>Rodrigo Diaz</h4>
+          <p>Solution Architect, Lorem Ipsum, Engineer Analyst, Programmer, Worker</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Instructorsec;
