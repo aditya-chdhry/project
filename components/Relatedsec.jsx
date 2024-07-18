5f8d40ee5f0efde8101fea7dc6d5e98d98d50706
Aditya Chaudhary <145284918+aditya-chdhry@users.noreply.github.com>
2024-07-18 15:17:32 +0530

    Create Relatedsec.jsx

---
+++ components/Relatedsec.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Relatedsec.css';
+
+const Relatedsec = () => {
+  return (
+    <div className="related-courses-section">
+      <h3>Related to Course</h3>
+      <ul>
+        <li>Classroom</li>
+        <li>Class Files</li>
+        <li>Certificates</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Relatedsec;
